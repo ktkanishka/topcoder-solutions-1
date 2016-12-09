bfc950b98e6e25e579c6fdaf1cbb365c84a6a93e
Moshe Shwarzberg <mosheshwarzberg@gmail.com>
2016-12-09 16:56:50 -0500

    Added z_array to project

---
+++ z_array.py
@@ -0,0 +1,40 @@
+def construct_z_array(string):
+    z_array = [0 for _ in string]
+    for i in xrange(1, len(string)):
+        j = i
+        k = 0
+        while j < len(string) and string[k] == string[j]:
+            j += 1
+            k += 1
+        z_array[i] = j - i
+    return z_array
+
+def construct_z_array_efficiently(string):
+    z_array = [0 for _ in string]
+    left, right = 0, 0
+    for i in xrange(1, len(string)):
+        if i >= right:
+            j = i
+            k = 0
+            while j < len(string) and string[k] == string[j]:
+                j += 1
+                k += 1
+            length = j - i
+            z_array[i] = length
+            if length > 0:
+                left = i
+                right = left + length - 1
+        else:
+            j = i + min(right - i, z_array[i - left])
+            k = min(right - i, z_array[i - left]) 
+            length = 1 if string[k] == string[j] else 0
+            print 'j: {}, i: {}, right: {}, left: {},  {}, z_array[i - left]: {}'.format(j, i, right, left, string[:i], z_array[i - left])
+            while j < len(string) and string[k] == string[j]:
+                j += 1
+                k += 1
+            length = j - i
+            z_array[i] = length
+            if length > 0:
+                left = i
+                right = left + length - 1
+    return z_array
