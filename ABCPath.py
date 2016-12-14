dc399ec9539420e78ad244fc4edc71d054efeccb
Moshe Shwarzberg <ms@zenoradio.com>
2016-12-14 13:44:00 -0500

    Push preivous work

--- ABCPath.py
+++ ABCPath.py
@@ -1,8 +1,27 @@
+from collections import deque
+
 class ABCPath(object):
     def length(self, grid):
-        print construct_graph(grid)
+        graph = construct_graph(grid)
+        in_degree_array = {i:0 for i in graph.keys()}
+        for k, l in graph.iteritems():
+            for v in l:
+                in_degree_array[v] += 1
+        q = deque([i for i in in_degree_array.keys() if in_degree_array[i] == 0])
+        topological_sort = []
+        while q:
+            e = q.popleft()
+            topological_sort.append(e)
+            for v in graph[e]:
+                in_degree_array[v] -= 1
+                if in_degree_array[v] == 0:
+                    q.append(v)
+        print topological_sort
+        print in_degree_array
 
-#This constructs the graph. Not we need to find the longest path within this graph.
+'''
+:returns: `dict`
+'''
 def construct_graph(grid):
     graph = {(i, j):[] for i in range(len(grid)) for j in range(len(grid[0]))}
     for i in xrange(len(grid)):
